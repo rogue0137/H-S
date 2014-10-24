5319f515dfe31560e0e39a235baf7f4809687e08
Krystal Flores <krystal.flores@gmail.com>
2014-10-24 16:26:59 -0700

    Added first, last, phone, street, city, state, and zip fields to sign up and edit user

---
+++ db/migrate/20141024231904_add_address_to_users.rb
@@ -0,0 +1,9 @@
+class AddAddressToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip, :string
+    add_column :users, :string, :string
+  end
+end
